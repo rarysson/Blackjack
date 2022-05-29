69e51c7a2e3d684682a417f6defed1fb0d479874
Rarysson Guilherme <r_guilherme12@hotmail.com>
2022-05-29 12:59:31 -0300

    Add dealer 'IA'

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,28 @@ import { CardsHolder } from "./components/cardsHolder/CardsHolder";
 import { PlayerActions } from "./components/playerActions/PlayerActions";
 import { BoardMessage } from "./components/boardMessage/BoardMessage";
 
+const MAX_POINTS = 21;
+
 function App() {
   const [dealerCards, setDealerCards] = createSignal([]);
   const [playerCards, setPlayerCards] = createSignal([]);
-  const [gameState, setGameState] = createSignal({ gameFinished: false, winner: "" });
+  const [isFetchingDealerCards, setIsFetchingDealerCards] = createSignal(false);
+  const [playerPoints, setPlayerPoints] = createSignal(0);
+  const [gameState, setGameState] = createSignal({ gameFinished: false, winner: "", dealerTurn: false });
+  let dealerIdInterval;
 
   async function drawDealerCard(numberOfCards = 1) {
     try {
+      if (isFetchingDealerCards()) return;
+
+      setIsFetchingDealerCards(true);
+
       const cards = await drawCards(numberOfCards);
       setDealerCards([...dealerCards(), ...cards,])
     } catch {
       showAlertMessage("Failed to draw card");
+    } finally {
+      setIsFetchingDealerCards(false);
     }
   }
 
@@ -32,23 +43,39 @@ function App() {
   }
 
   function handlePlayerPointsChange(points) {
-    if (points === 21) {
-      setGameState({ gameFinished: true, winner: "Player" });
-    } else if (points > 21) {
-      setGameState({ gameFinished: true, winner: "Dealer" });
+    setPlayerPoints(points);
+
+    if (points === MAX_POINTS) {
+      setGameState({ gameFinished: true, winner: "Player", dealerTurn: false });
+    } else if (points > MAX_POINTS) {
+      setGameState({ gameFinished: true, winner: "Dealer", dealerTurn: false });
     }
   }
 
   function handleDealerPointsChange(points) {
-    console.log("dealer: ", points);
+    if (points === MAX_POINTS || (points > playerPoints() && gameState().dealerTurn)) {
+      setGameState({ gameFinished: true, winner: "Dealer", dealerTurn: false });
+      clearInterval(dealerIdInterval);
+    } else if (points > MAX_POINTS) {
+      setGameState({ gameFinished: true, winner: "Player", dealerTurn: false });
+      clearInterval(dealerIdInterval);
+    }
   }
 
   function handleHit() {
     drawPlayerCard();
   }
 
+  function handleStand() {
+    setGameState({ gameFinished: false, winner: "", dealerTurn: true });
+
+    dealerIdInterval = setInterval(() => {
+      drawDealerCard();
+    }, 2000);
+  }
+
   function handleRestart() {
-    setGameState({ gameFinished: false, winner: "" });
+    setGameState({ gameFinished: false, winner: "", dealerTurn: false });
     setPlayerCards([]);
     setDealerCards([]);
 
@@ -82,12 +109,16 @@ function App() {
       </Show>
 
       <Switch>
+        <Match when={gameState().dealerTurn}>
+          <BoardMessage message="Dealer turn" />
+        </Match>
         <Match when={!gameState().gameFinished}>
-          <PlayerActions onHit={handleHit} />
+          <PlayerActions onHit={handleHit} onStand={handleStand} />
         </Match>
         <Match when={gameState().gameFinished}>
           <BoardMessage
             message={`${gameState().winner} won!`}
+            showRestartBtn={true}
             onRestart={handleRestart}
           />
         </Match>
--- src/components/boardMessage/BoardMessage.jsx
+++ src/components/boardMessage/BoardMessage.jsx
@@ -1,10 +1,19 @@
+import { Show } from "solid-js";
+
 import styles from "./boardMessage.module.css";
 
 export function BoardMessage(props) {
   return (
     <div className={styles.board}>
       <p className={styles.message}>{props.message}</p>
-      <button className={styles.btn} onClick={props.onRestart}>Restart</button>
+      <Show when={props.showRestartBtn}>
+        <button
+          className={styles.btn}
+          onClick={props.onRestart}
+        >
+          Restart
+        </button>
+      </Show>
     </div>
   );
 }
